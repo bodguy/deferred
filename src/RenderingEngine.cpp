4a1d8aca8543219f9925c5a0423bff6937d3b1d2
bodguy <alsrudgns99@gmail.com>
2019-10-30 22:02:23 +0900

    I finally fix it????????????? fucking asshole

--- src/RenderingEngine.cpp
+++ src/RenderingEngine.cpp
@@ -125,12 +125,11 @@ void RenderingEngine::initVertex() {
 }
 
 bool RenderingEngine::initFramebuffer() {
-  const unsigned int SHADOW_WIDTH = 1024, SHADOW_HEIGHT = 1024;
   // make depth framebuffer
   glGenFramebuffers(1, &depthMapFBO);
   glGenTextures(1, &depthMap);
   glBindTexture(GL_TEXTURE_2D, depthMap);
-  glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT16, SHADOW_WIDTH, SHADOW_HEIGHT, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT16, width, height, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
